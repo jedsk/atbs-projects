8225f06272679d77453252b5e4b478f08be780d2
jedsk <55721919+jedsk@users.noreply.github.com>
2022-01-09 13:55:05 -0500

    Update and rename ATBS - Strong PW Detection.ipynb to ch7_pw_detection.py

---
+++ ch7_pw_detection.py
@@ -0,0 +1,43 @@
+### Project - Strong Password Detection
+#>= 8 characters, Upper and lower, and has >= 1 digit
+
+import re
+
+char = re.compile(r'\S{8,}')
+lower_case = re.compile(r'[a-z]')
+upper_case = re.compile(r'[A-Z]')
+digit = re.compile(r'\d')
+                   
+
+
+Pass = False
+
+while Pass == False:
+    x = 0
+    print('Create your Password: \nMust contain at least 8 characters, a lower case, an upper case, and a digit')
+    pwInput = input()
+    
+    if char.search(pwInput) == None:
+        print('* Must be at least 8 characters')
+        x += 1
+        
+    if lower_case.search(pwInput) == None:
+        print('* Must contain one lower case')
+        x += 1
+        
+    if upper_case.search(pwInput) == None:
+        print('* Must contain an upper case')
+        x += 1
+        
+    if digit.search(pwInput) == None:
+        print('* Must contain a digit')
+        x += 1
+    
+    if x == 0:
+        print('Password Saved...')
+        break
+        
+    else: 
+        print('\n---Please Try Again---\n')
+           
+   
